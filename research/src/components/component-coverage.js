15b90c75ffd822cd3885a7a650186157f7f8c342
Levi Thomason <me@levithomason.com>
2019-09-10 21:01:13 -0700

    feat: cleanup detail pages (#7)
    * feat: cleanup detail pages
    
    * fix: header logo font
    
    * address PR comments, add sui checkbox images

--- research/src/components/component-coverage.js
+++ research/src/components/component-coverage.js
@@ -2,14 +2,34 @@ import React from 'react'
 import _ from 'lodash'
 import { componentsByName } from '../sources'
 
-const ComponentCoverage = ({ component }) => (
-  <ul>
-    {_.map(_.get(componentsByName, component), component => (
-      <li key={component.sourceName}>
-        {component.name} ({component.sourceName})
-      </li>
-    ))}
-  </ul>
-)
+const ComponentCoverage = ({ component }) => {
+  const matchingComponents = _.get(componentsByName, component)
+  const withDifferentNamesUniq = _.uniqBy(matchingComponents, 'name')
+  const withDifferentNamesGrouped = _.groupBy(matchingComponents, 'name')
+
+  if (_.isEmpty(withDifferentNamesUniq)) {
+    return null
+  }
+
+  return (
+    <div>
+      <h2>Names</h2>
+      <ul style={{ marginLeft: 0 }}>
+        {_.map(withDifferentNamesUniq, (component, i) => {
+          const components = withDifferentNamesGrouped[component.name]
+          const sourceNames = _.map(components, 'sourceName').join(', ')
+
+          return (
+            <li key={component.name} style={{ listStyleType: 'none' }}>
+              {component.name}
+              {' - '}
+              <small style={{ opacity: 0.75 }}>{sourceNames}</small>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
 
 export default ComponentCoverage
