d90881221c895b0c070496e292d249e2c04b3b8b
Levi Thomason <me@levithomason.com>
2019-09-27 12:34:55 -0700

    add anatomy grid helper

---
+++ research/src/components/anatomy-grid.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const borderColor = '#888'
+const borderScale = 2
+const bgColor = 'rgba(0, 0, 0, 0.1)'
+const textColor = '#fff'
+
+/**
+ * Given a CSS grid template, returns an array of grid areas in the order that they appear in the template.
+ */
+const getOrderedGridAreas = template =>
+  template
+    .split('\n')
+    .filter(s => s.includes('"'))
+    .map(s => s.slice(s.indexOf('"') + 1, s.lastIndexOf('"')))
+    .join(' ')
+    .split(/ +/)
+    .reduce((acc, next) => {
+      if (next === '' || next === '.' || acc.includes(next)) {
+        return acc
+      }
+      return [...acc, next]
+    }, [])
+
+const PartName = ({ children }) => (
+  <div
+    style={{
+      position: 'absolute',
+      paddingRight: borderScale,
+      paddingBottom: borderScale,
+      top: 0,
+      fontSize: '12px',
+      lineHeight: 1,
+      minWidth: '3ch',
+      textAlign: 'center',
+      color: textColor,
+      background: borderColor,
+      border: `${borderScale}px solid ${borderColor}`,
+    }}
+  >
+    {children}
+  </div>
+)
+PartName.propTypes = {
+  children: PropTypes.string.isRequired,
+}
+
+const Part = ({ gridArea, children }) => (
+  <div
+    style={{
+      gridArea: gridArea,
+      position: 'relative',
+      boxSizing: 'border-box',
+      margin: '4px',
+      border: `${borderScale}px solid ${borderColor}`,
+      background: bgColor,
+    }}
+  >
+    <PartName gridArea={gridArea}>{children}</PartName>
+  </div>
+)
+Part.propTypes = {
+  gridArea: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+}
+
+const Anatomy = ({ gridTemplate }) => {
+  const gridAreas = getOrderedGridAreas(gridTemplate)
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '1rem' }}>
+      <div
+        style={{
+          position: 'relative',
+          display: 'grid',
+          padding: '4px',
+          marginBottom: '1rem',
+          boxSizing: 'border-box',
+          width: '600px',
+          background: 'rgba(0, 0, 0, 0.1)',
+          border: `${borderScale}px solid ${borderColor}`,
+          gridTemplate,
+        }}
+      >
+        {gridAreas.map(gridArea => (
+          <Part key={gridArea} gridArea={gridArea}>
+            {gridArea}
+          </Part>
+        ))}
+      </div>
+
+      {gridAreas.length > 0 && (
+        <ol style={{ width: '500px' }}>
+          {gridAreas.map(name => (
+            <li key={name}>{name}</li>
+          ))}
+        </ol>
+      )}
+    </div>
+  )
+}
+Anatomy.propTypes = {
+  gridTemplate: PropTypes.string.isRequired,
+}
+
+export default Anatomy
--- research/src/components/carousel-anatomy.js
+++ research/src/components/carousel-anatomy.js
@@ -1,69 +1,16 @@
 import React from 'react'
+import AnatomyGrid from './anatomy-grid'
 
-const CarouselAnatomy = () => {
-  const titleStyles = {
-    fontSize: '10px',
-    lineHeight: '0.8',
-    position: 'absolute',
-    top: '0px',
-  }
-  const itemStyles = {
-    border: '1px dotted grey',
-    margin: '2px',
-    boxSizing: 'border-box',
-    position: 'relative',
-  }
-  const itemNextStyles = {
-    gridArea: 'next',
-  }
-  const itemPrevStyles = {
-    gridArea: 'prev',
-  }
-  const itemSlideStyles = {
-    gridArea: 'slide',
-  }
-  const itemRotationStyles = {
-    gridArea: 'rotation',
-  }
-  const itemPickerStyles = {
-    gridArea: 'picker',
-  }
-
-  const containerStyles = {
-    width: '242px',
-    margin: '10px 0 10px 0',
-    boxSizing: 'border-box',
-    display: 'grid',
-    border: '1px solid grey',
-    gridTemplateColumns: '40px 40px 40px 40px 40px 40px',
-    gridTemplateRows: '20px 40px 40px 40px 40px',
-    gridTemplateAreas: `
-    ". . . . . ."
-    ". slide slide slide slide ."
-    "prev slide slide slide slide next"
-    ". slide slide slide slide ."
-    ". rotation picker picker picker ."`,
-  }
-
-  return (
-    <div style={containerStyles}>
-      <div style={{ ...itemStyles, ...itemPrevStyles }}>
-        <small style={titleStyles}>prev</small>
-      </div>
-      <div style={{ ...itemStyles, ...itemSlideStyles }}>
-        <small style={titleStyles}>slide</small>
-      </div>
-      <div style={{ ...itemStyles, ...itemNextStyles }}>
-        <small style={titleStyles}>next</small>
-      </div>
-      <div style={{ ...itemStyles, ...itemRotationStyles }}>
-        <small style={titleStyles}>rotation</small>
-      </div>
-      <div style={{ ...itemStyles, ...itemPickerStyles }}>
-        <small style={titleStyles}>picker</small>
-      </div>
-    </div>
-  )
-}
+const CarouselAnatomy = () => (
+  <AnatomyGrid
+    gridTemplate={`
+      ".    slide    slide  slide  slide  .   " 80px
+      "prev slide    slide  slide  slide  next" 60px
+      ".    slide    slide  slide  slide  .   " 80px
+      ".    rotation picker picker picker .   " 60px /
+       60px 60px     1fr    1fr    80px   60px
+    `}
+  />
+)
 
 export default CarouselAnatomy
