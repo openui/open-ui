3854a99ce61aa6c31e724faefba4b4b11294f19a
Levi Thomason <me@levithomason.com>
2020-03-06 12:33:45 -0800

    feat: simplify data and pages (#48)
    * update name vs category
    
    * remove component category
    
    * publish research and proposal pages
    
    * fix contributing layout issues
    
    * improve typography
    
    * address pr comments

--- research/src/components/select-anatomy.js
+++ research/src/components/select-anatomy.js
@@ -3,14 +3,16 @@ import './select-anatomy.css'
 
 const SelectAnatomy = () => {
   return (
-    <host>
-      <part name="button">
-        <slot>{'Default = <button>${value}<span part="icon-arrow"></span></button>'}</slot>
-      </part>
-      <part name="pop-up">
-        <slot>{'Default = <option></option>'}</slot>
-      </part>
-    </host>
+    <div className="component-anatomy">
+      <host>
+        <part name="button">
+          <slot>{'Default = <button>${value}<span part="icon-arrow"></span></button>'}</slot>
+        </part>
+        <part name="pop-up">
+          <slot>{'Default = <option></option>'}</slot>
+        </part>
+      </host>
+    </div>
   )
 }
 
