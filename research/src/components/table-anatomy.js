d2a2d90cae80d6ccaf3ac859b9a6b19f9193c205
Levi Thomason <me@levithomason.com>
2020-03-19 17:49:44 -0700

    fix styles and page links

--- research/src/components/table-anatomy.js
+++ research/src/components/table-anatomy.js
@@ -1,74 +1,43 @@
 import React from 'react'
+import './anatomy.css'
 import './table-anatomy.css'
 
 const TableAnatomy = () => {
   return (
-    <tableComponent anatomy="part">
-      <tableHeader anatomy="part">
-        <tableTitle anatomy="part"></tableTitle>
-      </tableHeader>
-      <tableHeaderRow anatomy="part" class="flex grow">
-        <tableHeaderCell name="1" anatomy="part">
-          {'HeaderCellContent'}
-        </tableHeaderCell>
-        <tableHeaderCell name="2" anatomy="part">
-          {'HeaderCellContent'}
-        </tableHeaderCell>
-        <tableHeaderCell name="3" anatomy="part">
-          {'HeaderCellContent'}
-        </tableHeaderCell>
-        <tableHeaderCell name="4" anatomy="part">
-          {'HeaderCellContent'}
-        </tableHeaderCell>
-      </tableHeaderRow>
-      <tableRow name="1" anatomy="part" class="flex grow">
-        <tableCell name="1" anatomy="part">
-          {'CellContent'}
-        </tableCell>
-        <tableCell name="2" anatomy="part">
-          {'CellContent'}
-        </tableCell>
-        <tableCell name="3" anatomy="part">
-          {'CellContent'}
-        </tableCell>
-        <tableCell name="4" anatomy="part">
-          {'CellContent'}
-        </tableCell>
-      </tableRow>
-      <tableRow name="2" anatomy="part" class="flex grow">
-        <tableCell name="1" anatomy="part">
-          {'CellContent'}
-        </tableCell>
-        <tableCell name="2" anatomy="part">
-          {'CellContent'}
-        </tableCell>
-        <tableCell name="3" anatomy="part">
-          {'CellContent'}
-        </tableCell>
-        <tableCell name="4" anatomy="part">
-          {'CellContent'}
-        </tableCell>
-      </tableRow>
-      <tableFooter anatomy="part">
-        <tablePagination anatomy="part" class="flex grow">
-          <pageItem name="1" anatomy="part">
-            1
-          </pageItem>
-          <pageItem name="2" anatomy="part">
-            2
-          </pageItem>
-          <pageItem name="3" anatomy="part">
-            3
-          </pageItem>
-          <pageItem name="4" anatomy="part">
-            4
-          </pageItem>
-          <pageItem name="5" anatomy="part">
-            5
-          </pageItem>
-        </tablePagination>
-      </tableFooter>
-    </tableComponent>
+    <div className="table-anatomy component-anatomy">
+      <host>
+        <part name="table-header">
+          <part name="table-title" />
+        </part>
+        <part name="table-header-row">
+          <part name="table-header-cell">Header cell content</part>
+          <part name="table-header-cell">Header cell content</part>
+          <part name="table-header-cell">Header cell content</part>
+          <part name="table-header-cell">Header cell content</part>
+        </part>
+        <part name="table-row">
+          <part name="table-cell">Cell content 1</part>
+          <part name="table-cell">Cell content 2</part>
+          <part name="table-cell">Cell content 3</part>
+          <part name="table-cell">Cell content 4</part>
+        </part>
+        <part name="table-row">
+          <part name="table-cell">Cell content 1</part>
+          <part name="table-cell">Cell content 2</part>
+          <part name="table-cell">Cell content 3</part>
+          <part name="table-cell">Cell content 4</part>
+        </part>
+        <part name="table-footer">
+          <part name="table-pagination">
+            <part name="page-item">1</part>
+            <part name="page-item">2</part>
+            <part name="page-item">3</part>
+            <part name="page-item">4</part>
+            <part name="page-item">5</part>
+          </part>
+        </part>
+      </host>
+    </div>
   )
 }
 
