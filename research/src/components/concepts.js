682d3931d6b17372d01c3d74cbfe0ef38893ec68
Levi Thomason <me@levithomason.com>
2020-03-24 15:03:21 -0700

    clean up images

--- research/src/components/concepts.js
+++ research/src/components/concepts.js
@@ -5,15 +5,28 @@ import ConceptCoverage from './concept-coverage'
 import Specimens from './specimens'
 
 const Concepts = ({ component }) => {
+  const [showDescriptions, setShowDescriptions] = React.useState(false)
+
+  const conceptsForComponent = conceptsByComponent[component]
+
   return (
     <div>
-      {_.map(conceptsByComponent[component], (concept, conceptOpenUIName) => (
+      <div>
+        <input
+          type="checkbox"
+          checked={showDescriptions}
+          aria-checked={showDescriptions}
+          onChange={() => setShowDescriptions(!showDescriptions)}
+        />{' '}
+        Show descriptions
+      </div>
+      {_.map(conceptsForComponent, (concepts, conceptOpenUIName) => (
         <div key={conceptOpenUIName}>
           <h3 style={{ marginTop: '24px' }}>
             {conceptOpenUIName}{' '}
             <ConceptCoverage component={component} concept={conceptOpenUIName} />
           </h3>
-          <Specimens component={component} concept={conceptOpenUIName} />
+          <Specimens component={component} conceptName={conceptOpenUIName} concepts={concepts} />
         </div>
       ))}
     </div>
--- research/src/components/specimens.js
+++ research/src/components/specimens.js
@@ -3,8 +3,8 @@ import React from 'react'
 import { getImagesForComponentConcept } from '../sources'
 import Image from './image'
 
-const Specimens = ({ component, concept }) => {
-  const images = getImagesForComponentConcept(component, concept)
+const Specimens = ({ component, conceptName, concepts }) => {
+  const images = getImagesForComponentConcept(component, conceptName)
   const hasImages = !_.isEmpty(images)
 
   return (
@@ -16,41 +16,45 @@ const Specimens = ({ component, concept }) => {
         border: '1px solid #ccc',
       }}
     >
-      {images.map((image, i) => (
-        <div
-          key={i}
-          style={{
-            display: 'flex',
-            flex: '0 0 auto',
-            flexDirection: 'column',
-            padding: hasImages ? '8px' : '16px',
-            textAlign: 'center',
-          }}
-        >
-          <div style={{ margin: 'auto', padding: '4px' }}>
-            <Image
-              src={image.image}
-              title={image.image}
-              alt={`An image of the ${concept} concept on a ${component} component in ${image.sourceName}.`}
-            />
-          </div>
+      {images.map((image, i) => {
+        const hasOverrideName = image.name !== image.openUIName
+
+        return (
           <div
+            key={image.image}
             style={{
-              position: 'relative',
-              padding: '4px 8px',
-              minWidth: '120px',
-              textTransform: 'uppercase',
-              fontSize: '11px',
-              lineHeight: 1,
-              whiteSpace: 'nowrap',
-              color: 'rgba(0, 0, 0, 0.6)',
-              borderTop: '1px solid #ccc',
+              display: 'flex',
+              flex: '0 0 auto',
+              flexDirection: 'column',
+              padding: hasImages ? '8px' : '16px',
+              textAlign: 'center',
             }}
           >
-            {image.sourceName}
+            <div style={{ margin: 'auto auto 0 auto', padding: '4px' }}>
+              <Image
+                src={image.image}
+                title={image.image}
+                alt={`An image of the ${conceptName} concept on a ${component} component in ${image.sourceName}.`}
+              />
+            </div>
+            <div
+              style={{
+                position: 'relative',
+                padding: '4px 8px',
+                minWidth: '120px',
+                fontSize: '11px',
+                lineHeight: 1,
+                whiteSpace: 'nowrap',
+                color: 'rgba(0, 0, 0, 0.6)',
+                borderTop: '1px solid #ccc',
+              }}
+            >
+              <label style={{ textTransform: 'uppercase' }}>{image.sourceName}</label>
+              {hasOverrideName && <strong> "{image.name}"</strong>}
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   )
 }
