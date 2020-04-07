315ad7476181e28888ef2012c726f99bde0b1eb3
Greg Whitworth <gwhit@microsoft.com>
2020-04-07 11:14:25 -0700

    Updated anatomy

--- research/src/components/select-anatomy.js
+++ research/src/components/select-anatomy.js
@@ -4,12 +4,15 @@ import './anatomy.css'
 const SelectAnatomy = () => {
   return (
     <div className="component-anatomy">
-      <host>
+      <host class="select">
         <part name="button">
-          <slot>{'Default = <button>${value}<span part="icon-arrow"></span></button>'}</slot>
+          <slot>
+            Default = &lt;button&gt;currently selected value &lt;span part="icon-arrow"&gt;
+            &lt;/span&gt;&lt;/button&gt;
+          </slot>
         </part>
         <part name="pop-up">
-          <slot>{'Default = <option></option>'}</slot>
+          <slot>Required in this slot: one or more &lt;option&gt;&lt;/option&gt elements;</slot>
         </part>
       </host>
     </div>
