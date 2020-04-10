f4ad316a9b9374153e73db9860ff25225e9c63cc
Greg Whitworth <865244+gregwhitworth@users.noreply.github.com>
2020-04-10 13:22:12 -0700

    Updating the select specification to the template (#70)
    * Updated anatomy
    
    * initial updates to select and investigations into behaviors
    
    * Added open questions
    
    * Updated select spec to match template and other items
    
    * Update research/src/pages/select.proposal.mdx
    
    Co-Authored-By: Nicholas Rice <nicholas.rice119@gmail.com>
    
    Co-authored-by: Nicholas Rice <nicholas.rice119@gmail.com>

--- research/src/components/select-anatomy.js
+++ research/src/components/select-anatomy.js
@@ -4,13 +4,21 @@ import './anatomy.css'
 const SelectAnatomy = () => {
   return (
     <div className="component-anatomy">
-      <host>
-        <part name="button">
-          <slot>{'Default = <button>${value}<span part="icon-arrow"></span></button>'}</slot>
-        </part>
-        <part name="pop-up">
-          <slot>{'Default = <option></option>'}</slot>
-        </part>
+      <host class="select">
+        <slot>
+          <part name="button">
+            <slot>
+              Default = &lt;button&gt;currently selected value &lt;span part="icon-arrow"&gt;
+              &lt;/span&gt;&lt;/button&gt;
+            </slot>
+          </part>
+          <part name="pop-up">
+            <div class="element">
+              <div class="anatomy-label">&lt;optgroup&gt;</div>
+              <slot>Required in this slot: one or more &lt;option&gt;&lt;/option&gt elements;</slot>
+            </div>
+          </part>
+        </slot>
       </host>
     </div>
   )
