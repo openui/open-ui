7e95e60c63b83ecce43c73677dbed9e84d61dc30
Greg Whitworth <gwhit@microsoft.com>
2020-04-23 20:52:56 -0700

    Updated look of anatomy and added capability to toggle slots, fixes issue #75

--- research/src/components/select-anatomy.js
+++ research/src/components/select-anatomy.js
@@ -3,26 +3,30 @@ import './anatomy.css'
 
 const SelectAnatomy = () => {
   return (
-    <div className="component-anatomy">
-      <host class="select">
-        <slot>
-          <part name="button">
-            <slot>Currently selected option</slot>
-          </part>
-          <part name="listbox">
-            <slot>
-              <div class="element">
-                <div class="anatomy-label">optgroup</div>
-                <slot>
-                  <part name="option">
-                    <slot>Content</slot>
-                  </part>
-                </slot>
-              </div>
-            </slot>
-          </part>
-        </slot>
-      </host>
+    <div class="component-anatomy-wrapper">
+      <input type="checkbox" id="show-slots" />
+      <label for="show-slots"> Show slots</label>
+      <div class="component-anatomy">
+        <host name="select" data-slot>
+          <slot>
+            <part name="button" data-slot>
+              <slot>Currently selected option</slot>
+            </part>
+            <part name="listbox" data-slot>
+              <slot>
+                <div class="element">
+                  <div class="anatomy-label" name="optgroup" data-slot></div>
+                  <slot>
+                    <part name="option" data-slot>
+                      <slot>Content</slot>
+                    </part>
+                  </slot>
+                </div>
+              </slot>
+            </part>
+          </slot>
+        </host>
+      </div>
     </div>
   )
 }
