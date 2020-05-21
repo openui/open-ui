9ec11114912c884ee8c80371a5b7ef06796a2bd6
nicholasrice <nicholas.rice119@gmail.com>
2020-05-21 11:18:42 -0700

    aligning to template

--- research/src/components/checkbox-anatomy.js
+++ research/src/components/checkbox-anatomy.js
@@ -4,13 +4,17 @@ import './anatomy.css'
 export default () => {
   return (
     <div className="component-anatomy">
-      <host>
+      <host name="checkbox">
         <part name="label">
-          <slot>{'Default'}</slot>
+          <slot name="label"></slot>
         </part>
         <part name="checkbox">
-          <part name="checked-indicator" />
-          <part name="indeterminate-indicator" />
+          <part name="checked-indicator">
+            <slot></slot>
+          </part>
+          <part name="indeterminate-indicator">
+            <slot></slot>
+          </part>
         </part>
       </host>
     </div>
