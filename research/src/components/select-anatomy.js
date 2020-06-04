c6d59ff4d9ab42fc209d3cb9ac495a27f9f2c056
Greg Whitworth <gwhit@microsoft.com>
2020-06-04 13:51:37 -0700

    Updated cutton-container anatomy

--- research/src/components/select-anatomy.js
+++ research/src/components/select-anatomy.js
@@ -10,12 +10,14 @@ const SelectAnatomy = () => {
       <label for="show-slots"> Show slots</label>
       <div class="component-anatomy">
         <host name="select" data-slot>
-          <slot name="button-wrapper" data-slot>
+          <slot name="button-container" data-slot>
             <part name="button" data-slot>
-              <slot name="selected-value">Currently selected value</slot>
+              <slot name="selected-value">
+                <part name="selected-value">Currently selected value</part>
+              </slot>
             </part>
           </slot>
-          <slot name="listbox-wrapper" data-slot>
+          <slot name="listbox-container" data-slot>
             <part name="listbox" data-slot>
               <slot>
                 <div class="element">
