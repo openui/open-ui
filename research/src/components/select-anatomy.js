3e4b40202ba9b24469ce3d23a95f5dc959cd2206
Greg Whitworth <865244+gregwhitworth@users.noreply.github.com>
2020-06-12 23:13:05 -0700

    Updated anatomy with named outer slots (#99)
    * Updated anatomy with named outer slots. Also fixed a few warnings that ESLint was yelling about
    
    * Named the value slot
    
    * Updated cutton-container anatomy
    
    * Removed slots that are expected in elements of optgroup and option
    
    Co-authored-by: Greg Whitworth <gwhit@microsoft.com>

--- research/src/components/select-anatomy.js
+++ research/src/components/select-anatomy.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react'
 import './anatomy.css'
 
@@ -8,26 +10,22 @@ const SelectAnatomy = () => {
       <label for="show-slots"> Show slots</label>
       <div class="component-anatomy">
         <host name="select" data-slot>
-          <slot>
-            <slot data-slot>
-              <part name="button" data-slot>
-                <slot>Currently selected option</slot>
-              </part>
-            </slot>
-            <slot data-slot>
-              <part name="listbox" data-slot>
-                <slot>
-                  <div class="element">
-                    <div class="anatomy-label" name="optgroup" data-slot></div>
-                    <slot>
-                      <part name="option" data-slot>
-                        <slot>Content</slot>
-                      </part>
-                    </slot>
-                  </div>
-                </slot>
-              </part>
-            </slot>
+          <slot name="button-container" data-slot>
+            <part name="button" data-slot>
+              <slot name="selected-value">
+                <part name="selected-value">Currently selected value</part>
+              </slot>
+            </part>
+          </slot>
+          <slot name="listbox-container" data-slot>
+            <part name="listbox" data-slot>
+              <slot>
+                <div class="element">
+                  <div class="anatomy-label" name="optgroup" data-slot></div>
+                  <part name="option" data-slot></part>
+                </div>
+              </slot>
+            </part>
           </slot>
         </host>
       </div>
