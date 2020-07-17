8be14c77ce36c68ae215bb580154f5b92c934ea0
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-07-17 16:31:34 +0200

    feat(skeleton): add properties

---
+++ research/src/components/skeleton-anatomy.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+
+import React from 'react'
+import './anatomy.css'
+
+const SkeletonAnatomy = () => {
+  return (
+    <div class="component-anatomy-wrapper">
+      <input type="checkbox" id="show-slots" />
+      <label for="show-slots"> Show slots</label>
+      <div class="component-anatomy">
+        <host name="select" data-slot>
+          <slot name="button-container" data-slot>
+            <part name="button" data-slot>
+              <slot name="selected-value">
+                <part name="selected-value">Currently selected value</part>
+              </slot>
+            </part>
+          </slot>
+          <slot name="listbox-container" data-slot>
+            <part name="listbox" data-slot>
+              <slot>
+                <div class="element">
+                  <div class="anatomy-label" name="optgroup" data-slot></div>
+                  <part name="option" data-slot></part>
+                </div>
+              </slot>
+            </part>
+          </slot>
+        </host>
+      </div>
+    </div>
+  )
+}
+
+export default SkeletonAnatomy
