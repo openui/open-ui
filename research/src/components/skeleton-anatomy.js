c61568ce9c512da1c51e84c2a1330ce34cb75e6a
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-07-18 22:48:24 +0200

    feat(skeleton): skeleton anatomy

--- research/src/components/skeleton-anatomy.js
+++ research/src/components/skeleton-anatomy.js
@@ -6,27 +6,10 @@ import './anatomy.css'
 const SkeletonAnatomy = () => {
   return (
     <div class="component-anatomy-wrapper">
-      <input type="checkbox" id="show-slots" />
-      <label for="show-slots"> Show slots</label>
       <div class="component-anatomy">
-        <host name="select" data-slot>
-          <slot name="button-container" data-slot>
-            <part name="button" data-slot>
-              <slot name="selected-value">
-                <part name="selected-value">Currently selected value</part>
-              </slot>
-            </part>
-          </slot>
-          <slot name="listbox-container" data-slot>
-            <part name="listbox" data-slot>
-              <slot>
-                <div class="element">
-                  <div class="anatomy-label" name="optgroup" data-slot></div>
-                  <part name="option" data-slot></part>
-                </div>
-              </slot>
-            </part>
-          </slot>
+        <host name="skeleton" data-slot>
+          <part name="skeleton-line" data-slot></part>
+          <part name="skeleton-shape" data-slot></part>
         </host>
       </div>
     </div>
