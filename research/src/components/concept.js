5be29feeb6c3bcb7a1da54813feac2208691997c
Charles Assunção <junioassuncaocharles@gmail.com>
2020-08-22 00:14:13 +0200

    hotfix(concept): remove unecessary role from button (#164)

--- research/src/components/concept.js
+++ research/src/components/concept.js
@@ -39,7 +39,6 @@ export default function Concept({
             e.preventDefault()
             toggleOpen((isOpen) => !isOpen)
           }}
-          role="button"
         >
           <span
             style={{
