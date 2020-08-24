275dbf3a1bdac199aedebc7dfe03e99d0c62faac
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-08-24 11:38:13 +0200

    feat(proposal): add breadcrumb proposal

---
+++ research/src/components/breadcrumb-anatomy.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+
+import React from 'react'
+import './anatomy.css'
+import './breadcrumb-anatomy.css'
+
+const BreadcrumbAnatomy = () => {
+  return (
+    <div class="component-anatomy-wrapper">
+      <input type="checkbox" id="show-slots" />
+      <label for="show-slots"> Show slots</label>
+      <div class="component-anatomy">
+        <host name="breadcrumb">
+          <slot name="breadcrumb-container">
+            <part name="breadcrumb-item"></part>
+            <part name="breadcrumb-divider"></part>
+            <part name="breadcrumb-item"></part>
+          </slot>
+        </host>
+      </div>
+    </div>
+  )
+}
+
+export default BreadcrumbAnatomy
