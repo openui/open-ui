e63561dc64d03a3fcbfafb6e37486d1ba7896949
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-09-23 05:03:10 +0200

    feat(Breadcrumb): adapt anatomy

--- research/src/components/breadcrumb-anatomy.js
+++ research/src/components/breadcrumb-anatomy.js
@@ -10,12 +10,13 @@ const BreadcrumbAnatomy = () => {
       <input type="checkbox" id="show-slots" />
       <label for="show-slots"> Show slots</label>
       <div class="component-anatomy">
-        <host name="breadcrumb">
+        <host name="breadcrumb" role="nave">
           <slot name="breadcrumb-container">
-            <part name="breadcrumb-list-items">
-              <part name="breadcrumb-item"></part>
-              <part name="breadcrumb-divider"></part>
-              <part name="breadcrumb-item"></part>
+            <part name="ol">
+              <part name="li">item</part>
+              <part name="li" aria-hidden="true">
+                divider
+              </part>
             </part>
           </slot>
         </host>
