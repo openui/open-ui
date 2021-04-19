e40a2b6c1a7188b75beac6c5afb024f5ae70905d
HH: Andrico Karoulla <andrico@hhs-macbook-pro.home>
2021-04-19 20:32:42 +0100

    rename contribute to get started, add redirect from contribute url

---
+++ research/src/pages/contribute.js
@@ -0,0 +1,18 @@
+import { navigate } from 'gatsby'
+import { useEffect } from 'react'
+
+/**
+ *
+ * This component acts as a redirect to ensure backlinks aren't broken.
+ * Related: https://github.com/openui/open-ui/issues/303
+ */
+
+function ContributePage() {
+  useEffect(() => {
+    return navigate('/get-involved')
+  })
+
+  return null
+}
+
+export default ContributePage
