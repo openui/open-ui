2c878de4033731b5a6fe9295d8b8e1f84dad0c86
HH: Andrico Karoulla <andrico@hhs-macbook-pro.home>
2021-08-11 20:36:39 +0100

    bump dependencies

--- research/gatsby-config.js
+++ research/gatsby-config.js
@@ -34,37 +34,20 @@ module.exports = {
 
     'gatsby-plugin-sharp',
 
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: './src/images/logo-green.png',
-      },
-    },
-
     'gatsby-plugin-react-helmet',
 
-    // TODO: gatsby-plugin-manifest throws an error locally:
-    //       objc[60503]: Class GNotificationCenterDelegate is implemented in both
-    //         - sharp/vendor/lib/libgio-2.0.0.dylib (0x113160578)
-    //         - gatsby-plugin-manifest/node_modules/sharp/vendor/lib/libgio-2.0.0.dylib (0x1165c9578).
-    //       One of the two wil...
-    //
-    // TODO: It also throws another error on Netlify failing the deploy:
-    //       .nvm/versions/node/v12.10.0/bin/node: symbol lookup error:
-    //         node_modules/gatsby-plugin-manifest/node_modules/sharp/build/Release/sharp.node:
-    //           undefined symbol: _ZNK4vips6VImage7pngsaveEPKcPNS_7VOptionE
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: 'Open UI',
-    //     short_name: 'OUI',
-    //     start_url: '/',
-    //     background_color: '#fff',
-    //     theme_color: '#00a453',
-    //     display: 'minimal-ui',
-    //     icon: 'src/images/logo-green.png', // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Open UI',
+        short_name: 'OUI',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#00a453',
+        display: 'minimal-ui',
+        icon: 'src/images/logo-green.png', // This path is relative to the root of the site.
+      },
+    },
 
     'gatsby-plugin-remove-trailing-slashes',
 
