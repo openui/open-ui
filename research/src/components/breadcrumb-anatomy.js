b5a93184582e79fff59912e57f5f25309c09ba0a
Andrico <andrico.karoulla@healthhero.com>
2022-03-11 21:26:11 +0000

    add comments for future owner

--- research/src/components/breadcrumb-anatomy.js
+++ research/src/components/breadcrumb-anatomy.js
@@ -10,7 +10,7 @@ const BreadcrumbAnatomy = () => {
       <input type="checkbox" id="show-slots" />
       <label for="show-slots"> Show slots</label>
       <div class="component-anatomy">
-        <host name="breadcrumb" role="nav">
+        <host name="breadcrumb" role="navigation">
           <slot name="breadcrumb-container">
             <part name="ol">
               <part name="li [part=item]" data-slot>
