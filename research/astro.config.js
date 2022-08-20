ff46914132d2e68de130f18de28df105252c67ef
Andrico <andrico.karoulla@healthhero.com>
2022-08-20 19:14:27 +0100

    minor config change

--- research/astro.config.js
+++ research/astro.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config'
 
 import react from '@astrojs/react'
-import lit from '@astrojs/lit'
 import turbolinks from '@astrojs/turbolinks'
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), lit(), turbolinks(), sitemap()],
+  integrations: [react(), turbolinks(), sitemap()],
   site: 'https://open-ui.org',
   markdown: {
     shikiConfig: {
