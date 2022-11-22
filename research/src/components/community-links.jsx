a2e2db1df91153de498df1a09a733b2e62021082
Andrico <karoulla.andrico@gmail.com>
2022-11-22 22:36:16 +0000

    get build working

--- research/src/components/community-links.jsx
+++ research/src/components/community-links.jsx
@@ -2,23 +2,29 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons'
 
-const githubURL = ''
+const githubURL = 'https://github.com/openui/open-ui'
+
+const iconStyle = {
+  marginRight: '0.2em',
+  width: '16px',
+}
+
+const linkStyle = { color: 'inherit', display: 'flex', alignItems: 'center' }
 
 export default function CommunityLinks({ className = '' }) {
   return (
     <div className={'community-links ' + className}>
-      <a href={githubURL} target="_blank" rel="noreferrer noopener" style={{ color: 'inherit' }}>
-        <FontAwesomeIcon style={{ marginRight: '0.2em' }} icon={faGithub} /> GitHub
+      <a href={githubURL} target="_blank" rel="noreferrer noopener" style={linkStyle}>
+        <FontAwesomeIcon style={iconStyle} icon={faGithub} /> GitHub
       </a>
 
       <a
         href="https://discord.gg/DEWjhSw"
         target="_blank"
         rel="noreferrer noopener"
-        style={{ color: 'inherit' }}
+        style={linkStyle}
       >
-        <FontAwesomeIcon style={{ marginRight: '0.2em', marginLeft: '1em' }} icon={faDiscord} />{' '}
-        Discord
+        <FontAwesomeIcon style={{ ...iconStyle, marginLeft: '1em' }} icon={faDiscord} /> Discord
       </a>
     </div>
   )
