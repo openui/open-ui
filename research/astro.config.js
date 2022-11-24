9ee747339602776848bbde89ee42668e59ecc1b1
Andrico <karoulla.andrico@gmail.com>
2022-11-24 14:25:00 +0000

    remove unused dependencies, remove render blocking resources, prefetch resources

--- research/astro.config.js
+++ research/astro.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-import turbolinks from '@astrojs/turbolinks'
 import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
+import prefetch from '@astrojs/prefetch'
+import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), turbolinks(), sitemap(), mdx()],
+  integrations: [react(), sitemap(), mdx(), prefetch(), compress()],
   site: 'https://open-ui.org',
   markdown: {
     shikiConfig: {
--- research/src/components/image.jsx
+++ research/src/components/image.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export function Image({ alt, src, style }) {
-  return <img alt={alt} srcset={src + ' 2x'} style={style} />
+  return <img alt={alt} srcSet={src + ' 2x'} style={style} />
 }
 
 export default Image
