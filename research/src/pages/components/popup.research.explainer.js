fd28101b9f159ee6f30191c93d037db47bf17b36
Andrico <karoulla.andrico@gmail.com>
2023-01-15 11:57:14 +0000

    add redirect comment

--- research/src/pages/components/popup.research.explainer.js
+++ research/src/pages/components/popup.research.explainer.js
@@ -3,8 +3,9 @@ import { useEffect } from 'react'
 
 /**
  *
- * This component acts as a redirect to ensure backlinks aren't broken.
- * Related: https://github.com/openui/open-ui/issues/303
+ * This component acts as a redirect (from [1] to [2]) to ensure backlinks aren't broken.
+ * [1] https://open-ui.org/components/popup.research.explainer
+ * [2] https://open-ui.org/components/popover.research.explainer
  */
 
 function Page() {
