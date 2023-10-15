a174604bfdb5ef1370014438c8a7f6def5c6b30a
Luke Warlow <luke@warlow.dev>
2023-10-15 17:04:12 +0100

    Update selectlist anatomy
    - Change selectedvalue to selectedoption
    - Add marker part
    - Add optional legend part

--- site/src/components/select-anatomy.jsx
+++ site/src/components/select-anatomy.jsx
@@ -7,13 +7,15 @@ const SelectAnatomy = () => {
       <Host name="select">
         <Slot name="button">
           <Part name="button type=selectlist">
-            <Part name="selectedvalue">Currently selected value</Part>
+            <Part name="selectedoption">Currently selected option</Part>
+            <Part name="marker"></Part>
           </Part>
         </Slot>
         <Slot name="listbox">
           <Part name="listbox">
             <Slot>
               <Part name="optgroup (optional)">
+                <Part name="legend (optional)">Optgroup label</Part>
                 <Part name="option">Option text</Part>
               </Part>
             </Slot>
