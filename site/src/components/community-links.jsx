9e37d88dc2b905b34f3ef855b74b06f856438834
YummyBacon5 <68166338+YummyBacon5@users.noreply.github.com>
2023-10-31 19:00:49 +0000

    Add X to the community links

--- site/src/components/community-links.jsx
+++ site/src/components/community-links.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faDiscord, faGithub, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 import './community-links.css'
-
-const githubURL = 'https://github.com/openui/open-ui'
 
 const iconStyle = {
   marginRight: '0.2em',
@@ -13,12 +11,16 @@ const iconStyle = {
 export default function CommunityLinks({ className = '' }) {
   return (
     <div className={'community-links ' + className}>
-      <a href={githubURL} rel="noreferrer noopener">
+      <a href="https://github.com/openui/open-ui" rel="noreferrer noopener">
         <FontAwesomeIcon style={iconStyle} icon={faGithub} /> GitHub
       </a>
 
       <a href="https://discord.gg/DEWjhSw" rel="noreferrer noopener">
         <FontAwesomeIcon style={iconStyle} icon={faDiscord} /> Discord
+      </a>
+
+      <a href="https://x.com/openuicg" rel="noreferrer noopener">
+        <FontAwesomeIcon style={iconStyle} icon={faXTwitter} /> X
       </a>
     </div>
   )
