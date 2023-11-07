29f8c23fdb7922465b06d10bc916bd0eada0fb44
SUDHEER KUMAR REDDY GOWRIGARI <11438997+sudheer-gowrigari@users.noreply.github.com>
2023-11-07 15:31:45 -0800

    Combobox explainer initial anatomy proposal (#928)
    * Combobox explainer initial anatomy proposal
    
    * deleting duplicate image
    
    * updating contributors
    
    * updating contributors
    
    * Addressing review comments
    
    * linking issues with filter and search attributes
    
    * update defs

---
+++ site/src/components/combobox-anatomy.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { AnatomyWrapper, Host, Part, Slot } from './anatomy-components'
+
+const ComboboxAnatomy = () => {
+  return (
+    <AnatomyWrapper>
+      <Host name="combobox">
+        <Part name="input type=selectlist">
+          <Part name="selectedoption">Currently selected option</Part>
+        </Part>
+        <Slot name="listbox">
+          <Part name="listbox">
+            <Slot>
+              <Part name="optgroup (optional)">
+                <Part name="legend (optional)">Optgroup label</Part>
+                <Part name="option">Option text</Part>
+              </Part>
+            </Slot>
+          </Part>
+        </Slot>
+      </Host>
+    </AnatomyWrapper>
+  )
+}
+
+export default ComboboxAnatomy
