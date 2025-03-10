f8aa37600c56f8658b98aa140699ea173eca543c
brechtDR <deruytebrecht@gmail.com>
2025-03-10 10:52:43 +0100

    fix feedback as value with comma separataion does not seem to work

--- site/src/components/range-anatomy.jsx
+++ site/src/components/range-anatomy.jsx
@@ -11,9 +11,9 @@ const RangeAnatomy = () => {
             <Part name="range-segment">Segment 2</Part>
             <Part name="range-segment">Segment 3</Part>
           </Part>
-          <Part name="range-thumb">Thumb 1</Part>
-          <Part name="range-thumb">Thumb 2</Part>
-          <Part name="range-thumb">Thumb 3</Part>
+          <Part name="range-thumb">Thumb 1 (value=25)</Part>
+          <Part name="range-thumb">Thumb 2 (value=50)</Part>
+          <Part name="range-thumb">Thumb 3 (value=75)</Part>
           <Part name="range-tick">
             <Part name="range-tick-label">0%</Part>
           </Part>
