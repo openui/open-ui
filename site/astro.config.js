7d3e83f34688923b60c6d6fe53d246f5a485e218
Keith Cirkel <keithamus@users.noreply.github.com>
2026-06-04 12:47:16 +0100

    Normalise all explainer frontmatter

--- site/astro.config.js
+++ site/astro.config.js
@@ -7,8 +7,14 @@ import compress from 'astro-compress'
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 import { autolinkHeadingsPlugin } from './src/plugins/rehypeHeadings'
 import rehypeResponsiveTables from './src/plugins/rehypeResponsiveTable'
+import { remarkLastUpdated } from './src/plugins/remarkLastUpdated'
 
 export default defineConfig({
+  redirects: {
+    '/components/customizableselect/': '/components/customizable-select.explainer/',
+    '/components/customizableselect.explainer/': '/components/customizable-select.explainer/',
+    '/components/link-area-delegation-explainer/': '/components/link-area-delegation.explainer/',
+  },
   integrations: [
     react(),
     sitemap({
@@ -16,6 +22,7 @@ export default defineConfig({
         !page.endsWith("/component-spec-template/"),
     }),
     mdx({
+      remarkPlugins: [remarkLastUpdated],
       rehypePlugins: [rehypeHeadingIds, autolinkHeadingsPlugin, rehypeResponsiveTables],
     }),
     prefetch(),
